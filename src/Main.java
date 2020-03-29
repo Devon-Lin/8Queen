42432332b8f461f5901dbdf5b8de7f00f9aa169c
Devon Lin <devon_lin@Devons-MacBook-Pro.local>
2020-03-29 17:15:00 -0400

    Final solution

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,69 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
- 
 public class Main {
 
+	private static final int MAX_THREADS_COUNT = 10;
+
 	public static void main(String[] args) {
+		ThreadGroup threadGroup = new ThreadGroup("ThreadGroup");
+		int n = 0;
 
-		ThreadGroup tg1 = new ThreadGroup("ThreadGroup");
-		    int n = 0; 
-	        try (Scanner s=new Scanner(System.in)) {
-	        	while (true){
-	        		System.out.println("Enter the number of Queens :");
-	        		n = s.nextInt();
-	        		if ( n == 2 || n ==3) {
-	        			System.out.println("No Solution possible for "+ n +" Queens. Please enter another number");
-	        		}
-	        		else
-	        			break;
-	        	}
-	        }
-	        long timestamp1 = System.currentTimeMillis();
-	        
-	        System.out.println("Solution to "+ n +" queens using hill climbing search:");
-	        
-	        HillClimbingSearch hcs = new HillClimbingSearch(n);
-	        HillClimbingSearch hcs2 = new HillClimbingSearch(n);
-	        HillClimbingSearch hcs3 = new HillClimbingSearch(n);
-
-
-			Thread t1 = new Thread(tg1, hcs, "Name1");
-			Thread t2 = new Thread(tg1, hcs2, "Name2");
-			Thread t3 = new Thread(tg1, hcs3, "Name3");
-			t1.start();
-			t2.start();
-			t3.start();
-
-			//hcs.run();
-			System.out.println(tg1.activeCount());
-
-			while(tg1.activeCount() == 3){
-				System.out.println(tg1.activeCount());
-				System.out.println("Finding solution");
+		try (Scanner s=new Scanner(System.in)) {
+			while (true){
+				System.out.println("Enter the number of Queens :");
+				n = s.nextInt();
+				if ( n == 2 || n ==3) {
+					System.out.println("No Solution possible for "+ n +" Queens. Please enter another number");
+				}
+				else
+					break;
 			}
+		}
 
-			tg1.interrupt();
+		long timestamp1 = System.currentTimeMillis();
 
-	        if (hcs.getFinalSolution() != null)
-	        	hcs.printState(hcs.getFinalSolution());
-			if (hcs2.getFinalSolution() != null)
-				hcs2.printState(hcs2.getFinalSolution());
-			if (hcs3.getFinalSolution() != null)
-				hcs3.printState(hcs3.getFinalSolution());
+		System.out.println("Solution to "+ n +" queens using hill climbing search: ");
 
-	        //Printing the solution
-	        long timestamp2 = System.currentTimeMillis();
-			
-			long timeDiff = timestamp2 - timestamp1;
-			System.out.println("Execution Time: "+timeDiff+" ms");
+		List<HillClimbingSearch> hillClimbs = new ArrayList<>();
+		List<Thread> threads = new ArrayList<>();
 
-	    }
+		for(int i = 0; i < MAX_THREADS_COUNT; i++) {
+			var currHill = new HillClimbingSearch(n);
+			hillClimbs.add(currHill);
+			threads.add(new Thread(threadGroup, currHill, "Thread" + i));
+		}
+
+		threads.forEach(Thread::start);
+
+		while(threadGroup.activeCount() == threads.size()){
+			System.out.println("Finding solution (Active Threads: " + threadGroup.activeCount() + ")");
+		}
+
+		threadGroup.interrupt();
+		/*
+		hillClimbs.forEach(curr -> {
+			if(curr.getFinalSolution() != null) {
+				System.out.println("The solution found is: ");
+				curr.printState(curr.getFinalSolution());
+			}
+		});
+		*/
+
+		for (HillClimbingSearch hillClimb : hillClimbs) {
+			if (hillClimb.getFinalSolution() != null) {
+				System.out.println("The solution found is: ");
+				hillClimb.printState(hillClimb.getFinalSolution());
+			}
+		}
+
+
+		//Printing the solution
+		long timestamp2 = System.currentTimeMillis();
+
+		long timeDiff = timestamp2 - timestamp1;
+		System.out.println("Execution Time: "+timeDiff+" ms");
+		System.exit(0);
+	}
 }
